eabfb065f0c3d0a563a2adbb076cfb517d7e95e7
Irmo Manie <irmo.manie@gmail.com>
2019-12-02 10:36:54 +0100

    Add day 01

---
+++ src/main/scala/aoc/Day01.scala
@@ -0,0 +1,29 @@
+package aoc
+
+import scala.annotation.tailrec
+import scala.io.Source
+
+object Day01 extends App {
+
+  val source = Source.fromFile("input/day01.input")
+  val lines = source.getLines().toList
+
+  val totalFuel = lines.foldLeft(0){ case (total, mass) => total + computeFuel(mass.toInt)}
+  println(s"Total fuel: $totalFuel")
+
+  val totalFuelPlus = lines.foldLeft(0){ case (total, mass) => total + computeFuelPlus(mass.toInt)}
+  println(s"Total fuel plus: $totalFuelPlus")
+
+  @tailrec def computeFuelPlus(mass: Int, computed: Int = 0): Int = {
+    val fuel = math.max(computeFuel(mass), 0)
+    if (fuel > 0) {
+      computeFuelPlus(fuel, computed + fuel)
+    } else {
+      computed
+    }
+  }
+
+  @inline def computeFuel(mass: Int): Int = {
+    (mass / 3) - 2
+  }
+}
