f5f54cd4c9c2bf8d2bffcd306dd781effa6be07d
Irmo Manie <irmo.manie@gmail.com>
2019-12-05 11:05:36 +0100

    Fix next index operations

--- src/main/scala/aoc/Day05.scala
+++ src/main/scala/aoc/Day05.scala
@@ -43,9 +43,9 @@ object Day05 extends App {
       code.updated(getImmedateValueAt(index + 3), op(x, y))
     }
 
-    def nextIndexOperation(modes: List[Int]) = {
+    def nextIndexOperation(modes: List[Int], op: Int => Boolean) = {
       val param = getValue(index + 1, modes.headOption)
-      if (param != 0) getValue(index + 2, modes.drop(1).headOption) else index + 3
+      if (op(param)) getValue(index + 2, modes.drop(1).headOption) else index + 3
     }
 
     instruction match {
@@ -64,10 +64,10 @@ object Day05 extends App {
         val out = getValue(index + 1, modes.headOption)
         runOpCode(input, index + 2, code, output :+ out)
       case Instruction(5, modes) =>
-        val nextIndex = nextIndexOperation(modes)
+        val nextIndex = nextIndexOperation(modes, _ != 0)
         runOpCode(input, nextIndex, code, output)
       case Instruction(6, modes) =>
-        val nextIndex = nextIndexOperation(modes)
+        val nextIndex = nextIndexOperation(modes, _ == 0)
         runOpCode(input, nextIndex, code, output)
       case Instruction(7, modes) =>
         val updatedCode = xyOperation(modes, (x, y) => x < y)
