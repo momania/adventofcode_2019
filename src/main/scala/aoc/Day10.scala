01f7edcad50754367c9e350a82dd41e44107f388
Irmo Manie <irmo@nvision.ai>
2019-12-10 07:56:38 +0100

    Some more prep

--- src/main/scala/aoc/Day10.scala
+++ src/main/scala/aoc/Day10.scala
@@ -6,4 +6,24 @@ object Day10 extends App {
 
   val source = Source.fromFile("input/day10.input")
   val sourceCode = source.getLines().toList
+
+  val width = sourceCode.head.length
+  val height = sourceCode.length
+
+  println(s"Width: $width")
+  println(s"Height: $height")
+
+  case class Location(x: Int, y: Int, hasAstroid: Boolean)
+
+  val locations = sourceCode.zipWithIndex.map { case (line, y) =>
+    line.zipWithIndex.map { case (pos, x) => Location(x, y, (pos == '#')) }
+  }
+
+  for (l <- locations) {
+    println(s"Location: $l")
+  }
+
+  // todo find mechanism to 'look around' for each astroid,
+  //  by walking edges and compute all coordinates on path
+  // then check if path contains astroid
 }
